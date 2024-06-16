76152098be3fdac6772f3518c0223069b4f9780c
woody6624 <woody6624@gmail.com>
2024-06-16 19:01:51 +0900

    sd

--- src/main/java/ios_project_final/cheermassage/repository/UserRepository.java
+++ src/main/java/ios_project_final/cheermassage/repository/UserRepository.java
@@ -2,8 +2,11 @@ package ios_project_final.cheermassage.repository;
 
 import ios_project_final.cheermassage.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserEntity,String> {
-    UserEntity findByKakaoIdAndUserPassword(String kakaoId, String userPassword);
+    @Query("SELECT u FROM UserEntity u WHERE u.kakaoId = :kakaoId AND u.userPassword = :userPassword")
+    UserEntity findByKakaoIdAndUserPassword(@Param("kakaoId") String kakaoId, @Param("userPassword") String userPassword);
 
 }
