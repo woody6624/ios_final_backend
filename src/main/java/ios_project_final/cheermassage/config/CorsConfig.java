00e025607359cc086d442fa0fb14cf3de718fdd3
woody6624 <woody6624@gmail.com>
2024-06-16 14:37:37 +0900

    a

--- src/main/java/ios_project_final/cheermassage/config/CorsConfig.java
+++ src/main/java/ios_project_final/cheermassage/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://13.209.7.161")  // http://13.209.7.161에서 오는 모든 요청 허용
+                .allowedOriginPatterns("http://localhost")  // http://13.209.7.161에서 오는 모든 요청 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600);
